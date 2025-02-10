a1e7ef26b7c74f03c25bf3b84f0e3cf3dc53a996
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-10 19:50:28 +0530

    Update Wipro24_1.java

--- Wipro24_1.java
+++ Wipro24_1.java
@@ -1,3 +1,10 @@
+/*
+You are given a single character c. The character c lies between la - 2] or [A - Z].
+If the character is in the upper case then you are required to change that to lower case 
+or in the lower case then you are required to change it to upper case and print the same.
+
+*/
+
 import java.util.Scanner;
 class Wipro24_1
 {
